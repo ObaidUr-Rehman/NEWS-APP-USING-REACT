37638209f6680492150ff22307ac61c92d71c0af
Obaid Ur Rehman <obaidurrehman8121@gmail.com>
2022-10-17 17:00:32 +0300

    news api calling

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import News from "./News";
+
+const App = () => {
+  return (
+    <>
+    <h1 style={{textAlign:"center", backgroundColor: "black", color:"white"}}> NEWS API DATA</h1>
+    <News></News>
+    </>
+  )
+}
+export default App;
