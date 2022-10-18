73562cf309bca4c51528b393768f4d0ad467da99
Obaid Ur Rehman <96888672+ObaidUr-Rehman@users.noreply.github.com>
2022-10-18 23:22:28 +0300

    Update News.jsx

--- src/News.jsx
+++ src/News.jsx
@@ -1,11 +1,10 @@
 import React from "react";
 import { useEffect, useState } from "react";
 
-
 const News = () => {
+    
     const [data, setdata] = useState([]);
 
-    
     const getnewsapi = async () => {
         
         const res = await fetch("https://newsapi.org/v2/everything?q=apple&from=2022-10-16&to=2022-10-16&sortBy=popularity&apiKey=dbe4fa12db134b959d4c95136948ef4a");
@@ -15,6 +14,7 @@ const News = () => {
         console.log(data);
         
     };
+    
     
     useEffect(() => {
         getnewsapi();
@@ -29,33 +29,33 @@ const News = () => {
             const date = new Date(elem.publishedAt)
             const mydate = date.toLocaleDateString();
             return(
-        <div className='main'>
+                 <div className='main'>
 
-            <div className='about-post'>
-                <div className='info'>
-                    <h3 className='name'>{elem.title}</h3>
-                    <h5 className='time'>Date: {mydate}</h5>
-                </div>
+                   <div className='about-post'>
+                      <div className='info'>
+                         <h3 className='name'>{elem.title}</h3>
+                         <h5 className='time'>Date: {mydate}</h5>
+                       </div>
+                    </div>
+    
 
-
-            </div>
-
-            <div className='para'>
-                <p>{elem.description}</p>
-            </div>
-
-
-            <div className='postimg'>
-                <img src={elem.urlToImage} alt="" />
-            </div>
-
-            <div className="link">
-                <a href={elem.url} target="_blank"><button className="mybtn">Read More</button></a>
-            </div>
+                     <div className='para'>
+                        <p>{elem.description}</p>
+                      </div>
 
 
+                      <div className='postimg'>
+                         <img src={elem.urlToImage} alt="" />
+                       </div>
+    
 
-        </div>
+                        <div className="link">
+                            <a href={elem.url} target="_blank"><button className="mybtn">Read More</button></a>
+                         </div>
+
+
+
+                   </div>
             )
         
 
